ac8353412567fd3c489d6e1ebc192f6aca290470
Gabriel Fonseca <gabrieldfnsc@gmail.com>
2023-08-27 14:34:00 -0700

    feat(auth): setup laravel passport

--- app/Http/Controllers/Socialite/YouTubeSocialiteController.php
+++ app/Http/Controllers/Socialite/YouTubeSocialiteController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Socialite;
 
 use App\Http\Controllers\Controller;
+use App\Models\User as UserModel;
 use App\Services\Account\Auth\SocialiteInput;
 use App\Services\Account\Auth\SocialiteService;
 use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 use Laravel\Socialite\Contracts\User;
 use Laravel\Socialite\Facades\Socialite;
+use Symfony\Component\HttpFoundation\Cookie as SymfonyCookie;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class YoutubeSocialiteController extends Controller
@@ -27,9 +30,10 @@ class YoutubeSocialiteController extends Controller
         $user = Socialite::driver('google')->user();
         $input = $this->getInput($user);
         $user = $this->socialiteService->handle($input);
+        $cookie = $this->getCookie($user);
         Auth::login($user, true);
 
-        return redirect('dashboard');
+        return redirect('dashboard')->withCookie($cookie);
     }
 
     private function getInput(User $user): SocialiteInput
@@ -39,5 +43,12 @@ class YoutubeSocialiteController extends Controller
             email: $user->getEmail(),
             avatar: $user->getAvatar()
         );
+    }
+
+    private function getCookie(UserModel $user): SymfonyCookie
+    {
+        $accessToken = $user->createToken('spa-token')->accessToken;
+
+        return Cookie::make('token', $accessToken, 100080, null, null, false, false);
     }
 }
